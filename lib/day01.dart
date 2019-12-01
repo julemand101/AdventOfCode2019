ad2a788a5167fb7cf8b272b32a78abdf8d6eda05
Jacob Bang <julemand101@archlinux.dk>
2019-12-01 06:25:18 +0100

    Day01: Solved

---
+++ lib/day01.dart
@@ -0,0 +1,15 @@
+// --- Day 1: The Tyranny of the Rocket Equation ---
+// https://adventofcode.com/2019/day/1
+
+int solveA(Iterable<String> input) =>
+    input.map(int.parse).map(calcFuel).reduce((i1, i2) => i1 + i2);
+
+int solveB(Iterable<String> input) =>
+    input.map(int.parse).map(calcFuelRecursive).reduce((i1, i2) => i1 + i2);
+
+int calcFuel(int mass) => (mass / 3).floor() - 2;
+
+int calcFuelRecursive(int mass) {
+  final result = calcFuel(mass);
+  return result > 0 ? result + calcFuelRecursive(result) : 0;
+}
---
+++ test/day01_test.dart
@@ -0,0 +1,42 @@
+// --- Day 1: The Tyranny of the Rocket Equation ---
+// https://adventofcode.com/2019/day/1
+
+import 'dart:io';
+import 'package:test/test.dart';
+import 'package:AdventOfCode2019/day01.dart';
+
+const String dataFilePath = 'test/data/day01.txt';
+
+void main() {
+  group('Part One', () {
+    test('Example 1', () {
+      expect(solveA(const ['12']), equals(2));
+    });
+    test('Example 2', () {
+      expect(solveA(const ['14']), equals(2));
+    });
+    test('Example 3', () {
+      expect(solveA(const ['1969']), equals(654));
+    });
+    test('Example 4', () {
+      expect(solveA(const ['100756']), equals(33583));
+    });
+    test('Solution', () {
+      expect(solveA(File(dataFilePath).readAsLinesSync()), equals(3152375));
+    });
+  });
+  group('Part Two', () {
+    test('Example 1', () {
+      expect(solveB(const ['14']), equals(2));
+    });
+    test('Example 2', () {
+      expect(solveB(const ['1969']), equals(966));
+    });
+    test('Example 3', () {
+      expect(solveB(const ['100756']), equals(50346));
+    });
+    test('Solution', () {
+      expect(solveB(File(dataFilePath).readAsLinesSync()), equals(4725720));
+    });
+  });
+}
