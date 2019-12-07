77e9cf632af69584895d47a0f6e503988432c8e4
Jacob Bang <julemand101@archlinux.dk>
2019-12-07 12:29:33 +0100

    Day07a: Simplify combination generation

--- lib/day07.dart
+++ lib/day07.dart
@@ -8,9 +8,7 @@ int solveA(String input) {
   final memory = IntcodeComputer.parse(input);
   var maxThrusterSignal = 0;
 
-  for (final combinationIterable in combinations(List.generate(5, (i) => i))) {
-    final combination = combinationIterable.toList(growable: false);
-
+  for (final combination in combinations(List.generate(5, (i) => i))) {
     final amqA = IntcodeComputer(memory).compute([combination[0], 0]);
     final amqB = IntcodeComputer(memory).compute([combination[1], amqA.first]);
     final amqC = IntcodeComputer(memory).compute([combination[2], amqB.first]);
@@ -23,9 +21,9 @@ int solveA(String input) {
   return maxThrusterSignal;
 }
 
-Iterable<Iterable<int>> combinations(Iterable<int> inputs) sync* {
+Iterable<List<int>> combinations(Iterable<int> inputs) sync* {
   if (inputs.length == 1) {
-    yield inputs;
+    yield inputs.toList();
   } else {
     for (final input in inputs) {
       for (final combine in combinations(inputs.where((x) => x != input))) {
